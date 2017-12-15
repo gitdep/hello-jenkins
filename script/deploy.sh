520ddb9ba380599aa2a1219c732097969c4cf728
gitdep <duyroka@yahoo.com>
2017-12-15 14:55:20 +0700

    deploy 3

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
  
 #ssh app@APP.SERVER.IP.ADDRESS <<EOF
-  cd ~/Desktop/github/hello-jenkins
-  #cd ~/hello-jenkins
+  #cd ~/Desktop/github/hello-jenkins
+  cd ~/hello-jenkins
   git pull
   npm install --production
   forever restartall
